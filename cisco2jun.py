cfa2c728258a1050c638208b6b2d9f1d084c736d
jtfuguet <jf867916@wcupa.edu>
2020-11-14 22:28:21 -0600

    Added load file capability

--- cisco2jun.py
+++ cisco2jun.py
@@ -1,1 +1,19 @@
+import sys, getopt
+
+#function to convert each line in the source file into a juniper-compliant firewall rule
+def convert(line):
+	print line
+
+if (len(sys.argv)!=3): #usage error, if num args != 3 then program was called incorrectly
+	print 'Usage: cisco2jun <source_file> <dest_file>'
+	quit()
+
+print 'Num args:', len(sys.argv)
+print 'Loading ACL source "', str(sys.argv[1]),'\"' 
+ACLSource = open(sys.argv[1], 'r')
+print 'Source loaded'
+
+for line in ACLSource:
+	convert(line)
+
 
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-readme
+Joe Fuguet
+November 14 2020
+Command line utility to convert Cisco IOS ACLs to Juniper compatible format
